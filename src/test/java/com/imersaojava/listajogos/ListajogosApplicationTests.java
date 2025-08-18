c42e0aa5d580ed46aa924012e02658f7e74b4666
Hugo Pamponet <hugofarias9@gmail.com>
2025-08-18 13:24:32 -0300

    Project created

---
+++ src/test/java/com/imersaojava/listajogos/ListajogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imersaojava.listajogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListajogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
