3a98a4a81e3e90ee4200f22abdc3995423ef39cc
Hugo Pamponet <hugofarias9@gmail.com>
2025-08-18 15:05:54 -0300

    Criação do GameController

---
+++ src/main/java/com/imersaojava/listajogos/controller/GameController.java
@@ -0,0 +1,26 @@
+package com.imersaojava.listajogos.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imersaojava.listajogos.entities.Game;
+import com.imersaojava.listajogos.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<Game> findAll() {
+		List<Game> result = gameService.findAll();
+		return result;
+	}
+
+}
