c579cbe45448c8464b1c01dd57ff224fd68ee193
Hugo Pamponet <hugofarias9@gmail.com>
2025-08-19 14:55:37 -0300

    Create GameDTO

--- src/main/java/com/imersaojava/listajogos/services/GameService.java
+++ src/main/java/com/imersaojava/listajogos/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.imersaojava.listajogos.DTO.GameDTO;
 import com.imersaojava.listajogos.entities.Game;
 import com.imersaojava.listajogos.repository.GameRepository;
 
@@ -13,6 +14,12 @@ public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
+	
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
 	
 	public List<Game> findAll() {
 		List<Game> result = gameRepository.findAll();
